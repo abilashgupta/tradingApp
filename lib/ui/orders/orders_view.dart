7fc4361b3eae3c9fe7513c0fd21d2095721b484a
abilashgupta <abilashgupta.97@gmail.com>
2021-08-26 11:47:09 +0530

    My order page null bug fix

--- lib/ui/orders/orders_view.dart
+++ lib/ui/orders/orders_view.dart
@@ -37,58 +37,61 @@ class Orders extends StatelessWidget {
           centerTitle: true,
         ),
         resizeToAvoidBottomInset: false,
-        body: Container(
-            margin: EdgeInsets.only(top: 20, left: 10),
-            child: Container(
-                child: ListView.builder(
-                    itemCount: 1,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        children: [
-                          Row(
+        body: (list == {} || orderType == '')
+            ? Center(child: Text('My Orders'))
+            : Container(
+                margin: EdgeInsets.only(top: 20, left: 10),
+                child: Container(
+                    child: ListView.builder(
+                        itemCount: 1,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(
                             children: [
-                              Text(
-                                'Company :  ',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                              Text(
-                                displayOrder[0]['name'],
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 5),
-                          Row(
-                            children: [
-                              Text('Price :  ', style: TextStyle(fontSize: 18)),
-                              Text(displayOrder[3].toString()),
-                              SizedBox(width: 20),
-                              Text('Quantity :  ',
-                                  style: TextStyle(fontSize: 18)),
-                              Text(displayOrder[2].toString()),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Container(
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 10, vertical: 5),
-                                  decoration: BoxDecoration(
-                                    color: (displayOrder[1] == 'Buy')
-                                        ? Colors.green
-                                        : Colors.red,
+                              Row(
+                                children: [
+                                  Text(
+                                    'Company :  ',
+                                    style: TextStyle(fontSize: 18),
                                   ),
-                                  child: Text(displayOrder[1])),
-                              SizedBox(width: 30),
-                              Text('Exchange:  ',
-                                  style: TextStyle(fontSize: 18)),
-                              Text(displayOrder[0]['exchange']),
+                                  Text(
+                                    displayOrder[0]['name'],
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  Text('Price :  ',
+                                      style: TextStyle(fontSize: 18)),
+                                  Text(displayOrder[3].toString()),
+                                  SizedBox(width: 20),
+                                  Text('Quantity :  ',
+                                      style: TextStyle(fontSize: 18)),
+                                  Text(displayOrder[2].toString()),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 10, vertical: 5),
+                                      decoration: BoxDecoration(
+                                        color: (displayOrder[1] == 'Buy')
+                                            ? Colors.green
+                                            : Colors.red,
+                                      ),
+                                      child: Text(displayOrder[1])),
+                                  SizedBox(width: 30),
+                                  Text('Exchange:  ',
+                                      style: TextStyle(fontSize: 18)),
+                                  Text(displayOrder[0]['exchange']),
+                                ],
+                              ),
+                              Divider(color: Colors.white, height: 25),
                             ],
-                          ),
-                          Divider(color: Colors.white, height: 25),
-                        ],
-                      );
-                    }))),
+                          );
+                        }))),
       ),
       viewModelBuilder: () => OrdersViewModel(),
     );
